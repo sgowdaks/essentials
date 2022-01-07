9b62f4cbbc7d615c8c97d60c0ea0d3bd21d94ffe
Shivani Gowda KS <56445524+sgowdaks@users.noreply.github.com>
2022-01-06 17:35:34 -0800

    Update promises.js

--- JSessentials/promises.js
+++ JSessentials/promises.js
@@ -42,3 +42,26 @@ buyFlightTicket()
 ).catch((error) => 
     console.log(error)
 )
+
+
+
+--------------------------------------------
+// async and await example
+
+const photos = [];
+
+async function photoUpload() {
+    let uploadStatus = new Promise( (resolve, reject) => {
+        setTimeout( () => {
+            photos.push("Profile Pic");
+            resolve("Photo Uploaded")
+        }, 3000)
+    })
+    
+    let result = await uploadStatus;
+    
+    console.log(result);
+    console.log(photos.length);
+}
+
+photoUpload();
